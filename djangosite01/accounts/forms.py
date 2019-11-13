efb7966db4b73cc3c16cb3c0ef7ef39f9473358f
thefisk <triphazard@gmail.com>
2019-11-13 18:37:35 +0000

    Added custom registration form

--- djangosite01/accounts/forms.py
+++ djangosite01/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
+from allauth.account.forms import SignupForm
+from predictor.models import Team
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -13,3 +15,14 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+    fav_team = forms.ModelChoiceField(queryset=Team.objects.all(), label='Favourite Team')
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.FavouriteTeam = self.fav_team
+        user.save()
+        return user
