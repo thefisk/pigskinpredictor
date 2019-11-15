d9d6f3a32c5375ad17af1a9351e67c168273d0a2
thefisk <triphazard@gmail.com>
2019-11-15 13:50:59 +0000

    installed django-heroku and gunicorn

--- djangosite01/accounts/adapters.py
+++ djangosite01/accounts/adapters.py
@@ -3,7 +3,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 class AccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=False):
         data = form.cleaned_data
-        #user.username = data['username']
         user.email = data['email']
         user.first_name = data['first_name']
         user.last_name = data['last_name']
