7802409a432a15bf34232250b10a7176211d9bb4
thefisk <triphazard@gmail.com>
2019-11-15 14:03:01 +0000

    Added static_root for Heroku deployment

--- djangosite01/djangosite01/settings.py
+++ djangosite01/djangosite01/settings.py
@@ -136,7 +136,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 LOGIN_URL= '/accounts/login'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
