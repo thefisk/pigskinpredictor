716c677c6efa58928b6c1b46497b91e65f396262
thefisk <triphazard@gmail.com>
2019-11-15 16:37:37 +0000

    updated settings for hosts and secret key

--- djangosite01/settings.py
+++ djangosite01/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-tzcufowunm%!)n80z)vjy=xog!7d)96y(^_w9lphx6tx!+1d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pigskinpredictor.herokuapp.com','pigskinpredictor.com']
 
 
 # Application definition
