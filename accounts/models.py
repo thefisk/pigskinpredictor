0da5a6cf00987b2ec8d4f4783da3669c72514568
Nathan Fisk <triphazard@gmail.com>
2020-09-09 15:08:20 +0100

    Force User names to Title Case

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     Full_Name = models.CharField(max_length=100, null=True, blank=True)
     
     def save(self, *args, **kwargs):
+        self.first_name = self.first_name.title()
+        self.last_name = self.last_name.title()
         self.Full_Name = str(self.first_name +" "+self.last_name)
         super(User, self).save(*args, **kwargs)
 
