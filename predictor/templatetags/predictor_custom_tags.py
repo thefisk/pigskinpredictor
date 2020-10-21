f000db999f89bd6c9825d7d98bd582946889350d
Nathan Fisk <triphazard@gmail.com>
2020-10-21 15:32:52 +0100

    Results Page Banker Highlighting

--- predictor/templatetags/predictor_custom_tags.py
+++ predictor/templatetags/predictor_custom_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.contrib.auth.models import Group
-from predictor.models import Match, ScoresWeek, Results, Team, ScoresSeason
+from predictor.models import Match, ScoresWeek, Results, Team, ScoresSeason, Prediction
 from accounts.models import User
 import os
 
@@ -64,6 +64,13 @@ def division_total(div):
         except:
             pass
     return total
+
+@register.filter(name='banker_class')
+def banker_class(banker):
+    if banker.Points > 0:
+        return "results-table-right-banker"
+    else:
+        return "results-table-wrong-banker"
 
 #@register.filter(name='seasonhigh')
 #def seasonhigh(user):
