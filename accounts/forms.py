c009e569ff7e37d1e2a9101fefde3abde0078dc2
Nathan Fisk <triphazard@gmail.com>
2021-09-16 13:36:35 +0100

    TimeZone to Register Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
+from django.forms.fields import ChoiceField
 from .models import User
 from allauth.account.forms import SignupForm
 from predictor.models import Team
 from django.contrib.auth import get_user_model
 from material import Layout, Row
+from .timezones import timezonelist
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -25,13 +27,20 @@ class CustomUserChangeForm(UserChangeForm):
         exclude = ('username', 'email')
 
 class CustomSignupForm(SignupForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        self.initial['timezone'] = 'Europe/London'
+
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
     fav_team = forms.ModelChoiceField(queryset=Team.objects.filter(Active=True), empty_label=None, label='Favourite Team')
+    timezone = forms.ChoiceField(choices = timezonelist, label='Timezone')
     layout = Layout('email',
                     Row('password1', 'password2'),
                     Row('first_name', 'last_name'),
-                             'fav_team')
+                    Row('fav_team', 'timezone')
+                    )
     
     class Meta:
         model = get_user_model()
@@ -40,5 +49,6 @@ class CustomSignupForm(SignupForm):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.FavouriteTeam = self.cleaned_data['fav_team']
+        user.Timezone = self.cleaned_data['timezone']
         user.save()
         return user
