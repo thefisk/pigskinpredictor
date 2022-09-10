f321839d9ce0199e76716146ece634e1196b3b62
thefisk <triphazard@gmail.com>
2022-09-10 18:20:58 +0300

    Removed old line, added extra commentary

--- predictor/tasks.py
+++ predictor/tasks.py
@@ -275,12 +275,14 @@ def populate_live():
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
     # test for week 1
     # tomorrow = datetime.datetime.fromisoformat('2021-09-12').date()
-    print(tomorrow)
+    print("tomorrow is "+tomorrow)
+    gamecount=0
     for game in Match.objects.filter(Season=int(os.environ['PREDICTSEASON'])):
-    # for game in Match.objects.filter(Season=2021):
         if game.DateTime.date() == tomorrow and game.DateTime.hour < 23:
             newlive = LiveGame(Game=game.GameID, HomeTeam=game.HomeTeam.ShortName, AwayTeam=game.AwayTeam.ShortName, KickOff=game.DateTime.strftime("%H%M"), TeamIndex=teamdict[game.AwayTeam.ShortName], State=3)
             newlive.save()
+            gamecount += 1
+    print(gamecount+" live games imported")
 
 
 @shared_task
